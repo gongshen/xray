0bf710dcbb280c207c4261a032562ea3f3ddb451
shen_gong <shen_gong@intsig.net>
2026-01-30 16:59:59 +0800

    optimize

--- gin-vue-admin/server/core/server.go
+++ gin-vue-admin/server/core/server.go
@@ -32,17 +32,18 @@ func RunWindowsServer() {
 	address := fmt.Sprintf(":%d", global.GVA_CONFIG.System.Addr)
 	s := initServer(address, Router)
 	// 保证文本顺序输出
-	// In order to ensure that the text order output can be deleted
 	time.Sleep(10 * time.Microsecond)
 	global.GVA_LOG.Info("server run success on ", zap.String("address", address))
 
 	fmt.Printf(`
 	欢迎使用 gin-vue-admin
-	当前版本:v2.5.5
-	插件市场:https://plugin.gin-vue-admin.com
-	GVA讨论社区:https://support.qq.com/products/371961
-	默认自动化文档地址:http://127.0.0.1%s/swagger/index.html
-	默认前端文件运行地址:http://127.0.0.1:8080
+	默认前端文件运行地址:http://127.0.0.1%s
 `, address)
-	global.GVA_LOG.Error(s.ListenAndServeTLS(global.GVA_CONFIG.System.CertFile, global.GVA_CONFIG.System.KeyFile).Error())
+
+	// 如果配置了证书文件，使用 HTTPS，否则使用 HTTP
+	if global.GVA_CONFIG.System.CertFile != "" && global.GVA_CONFIG.System.KeyFile != "" {
+		global.GVA_LOG.Error(s.ListenAndServeTLS(global.GVA_CONFIG.System.CertFile, global.GVA_CONFIG.System.KeyFile).Error())
+	} else {
+		global.GVA_LOG.Error(s.ListenAndServe().Error())
+	}
 }
