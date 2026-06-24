615bed11c6f193d5df00b787cd06f1d59a6d8a6e
shen_gong <shen_gong@intsig.net>
2026-06-24 11:10:46 +0800

    refactor: group user binding row actions

--- gin-vue-admin/web/src/view/v2ray/binding/binding.vue
+++ gin-vue-admin/web/src/view/v2ray/binding/binding.vue
@@ -36,7 +36,9 @@
         </el-table-column>
           <el-table-column align="left" label="按钮组">
             <template #default="scope">
-            <el-button type="primary" link icon="share" class="table-button" @click="shareBindingFunc(scope.row)">分享</el-button>
+                <div class="table-row-actions" role="group" aria-label="绑定行操作">
+                    <el-button type="primary" link icon="share" class="table-button" @click="shareBindingFunc(scope.row)">分享</el-button>
+                </div>
             </template>
         </el-table-column>
         </el-table>
